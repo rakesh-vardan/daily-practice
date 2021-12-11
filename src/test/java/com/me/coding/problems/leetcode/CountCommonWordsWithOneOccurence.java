ad8c33c1a43b3f2ee57ce6254e2eb06c82dcf856
Rakesh Vardan <rakeshlearnit@gmail.com>
2021-12-11 07:43:24 +0000

    changes to testng tests

--- src/test/java/com/me/coding/problems/leetcode/CountCommonWordsWithOneOccurence.java
+++ src/test/java/com/me/coding/problems/leetcode/CountCommonWordsWithOneOccurence.java
@@ -4,19 +4,21 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
  * Given two string arrays words1 and words2,
  * return the number of strings that appear exactly once in each of the two
- * arrays.
+ * arrays. For example:
+ * 
  * words1 = ["selenium", "is", "amazing", "as", "is"]
  * words2 = ["amazing", "selenium", "is"]
+ * 
+ * output = 2 (selenium & amazing)
  */
-public class CountCommonWordsWithOneOccurence {
 
-    String[] words1 = new String[] { "leetcode", "is", "amazing", "as", "is" };
-    String[] words2 = new String[] { "amazing", "leetcode", "is" };
+public class CountCommonWordsWithOneOccurence {
 
     public int findCount(String[] words1, String[] words2) {
 
@@ -50,9 +52,23 @@ public class CountCommonWordsWithOneOccurence {
         return presentOnceInAandB.size();
     }
 
-    @Test
-    public void testMe() {
-        Assert.assertEquals(findCount(words1, words2), 20);
+    @Test(dataProvider = "inputData")
+    public void testMe(String[] words1, String[] words2, int count) {
+        Assert.assertEquals(findCount(words1, words2), count);
+    }
 
+    @DataProvider(name = "inputData")
+    public Object[][] getData() {
+        return new Object[][] {
+                { new String[] { "selenium", "is", "amazing", "as", "is" }, new String[] { "amazing", "selenium", "is" }, 2 },
+
+                { new String[] { "selenium" }, new String[] { "selenium" }, 1 },
+
+                { new String[] { "cypress" }, new String[] { "selenium" }, 0 },
+
+                { new String[] { "selenium" }, new String[] {}, 0 },
+
+                { new String[] {}, new String[] {}, 0 },
+        };
     }
 }
